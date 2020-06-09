e135ac1b8c62be61bbd0c77a8e4ff4174413b09a
Shini Kurian <shinikurian009@gmail.com>
2020-06-09 19:21:52 +0530

    error

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit,Input } from '@angular/core';
+import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+hero :Hero;
+  constructor(private route:ActivatedRoute,
+              private heroSevice:HeroService,
+              private location:Location) { }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+  getHero():void{
+    const id=this.route.snapshot.paramMap.get('id');
+   this.heroSevice.getHero(parseInt(id)).subscribe
+   (hero=>this.hero=hero);
+  }
+  goBack():void{
+    this.location;
+  }
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import {Hero} from './hero';
+export const HEROES:Hero[]=[
+    {id:10,name:'aputy'},
+    {id:11,name:'kili'},
+    {id:12,name:'ponnu'},
+    {id:13,name:'appu'},
+     {id:14,name:'dia'}
+];
